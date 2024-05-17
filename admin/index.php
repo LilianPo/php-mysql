82a7d0a9aeac97ba3bced840b6532244380518c0
LilianPo <156500287+LilianPo@users.noreply.github.com>
2024-05-17 10:48:09 +0300

    17.05

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<?php
+
+print_r(password_hash("admin123", PASSWORD_DEFAULT));
+
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    die("Vale koht");
+}
+
+?>
+<h1>ERITI SALAJANE</h1>
+<a href="logout.php">Logi välja</a>
